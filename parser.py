4baf3888f29a25cd55e3169fe8a93ee7c8d9af95
Borat <b.yelmurzayev@gmail.com>
2021-03-05 00:03:41 +0600

    write on list

--- parser.py
+++ parser.py
@@ -1,14 +1,11 @@
 
 import csv
-import types
+
 import datetime
 
 
 
-r = types.SimpleNamespace()
-r.entry_time = []
-r.exit_time = []
-r.person = []
+
 
 
 
@@ -18,22 +15,26 @@ with open("test.csv", encoding='utf-8') as r_file:
     # Счетчик для подсчета количества строк и вывода заголовков столбцов
     count = 0
     # Считывание данных из CSV файла
+    d = {
+       'time': [],
+       'person': [], 
+       'company': [] 
+    }
 
     for row in file_reader:
         if count == 0:
             # Вывод строки, содержащей заголовки для столбцов
             print(f'Файл содержит столбцы: {", ".join(row)}')
-            
+            count += 1
         else:
+            d = {
+                    'time': row[0],
+                    'person': row[1] + ' ' + row[2], 
+                    'company': row[4] 
+            }
             
-            filter_time(row[0])
-            
-            
-        count += 1
-    print(r.entry_time)
+        
+    
     print(f'Всего в файле {count} строк.')
-
-#  23.10.2020 19:41:01 000
-def filter_time(time_string):
-    date_time_str = datetime.datetime.strptime(time_string, '%d.%m.%Y %H:%M:%S')
+    print(d)
     
