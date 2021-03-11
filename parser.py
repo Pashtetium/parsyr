c0563fdbb06227a2107394ba0afdd3da3c3fe85a
Pashtetium <54268487+Pashtetium@users.noreply.github.com>
2021-03-11 17:59:54 +0600

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,40 +1,67 @@
-
 import csv
+import json
+from datetime import datetime as dt
 
-import datetime
+data = {}       # Основное хранилище
+
+rows_to_skip = 1   # Переменные для пропуска первой строки в csv файле
+rows_read = 0      #
+
+#Отрезаем лишние символы в строке времени ("23.10.2020 19:41:01 000")
+def time_slicing(end_date):
+    return end_date[:end_date.rindex(" ")]
+
+#Преобразывываем обрезанную строку в формат datetime
+def str_to_datetime(sliced_str):
+    return dt.strptime(sliced_str, "%d.%m.%Y %H:%M:%S")
+
+#def comparingTime(time_str, person):
+#    for time_str in data:
+ #       if data[person]
 
 
 
-
-
-
-
-with open("test.csv", encoding='utf-8') as r_file:
-    # Создаем объект reader, указываем символ-разделитель ","
-    file_reader = csv.reader(r_file, delimiter = ";")
-    # Счетчик для подсчета количества строк и вывода заголовков столбцов
-    count = 0
-    # Считывание данных из CSV файла
-    d = {
-       'time': [],
-       'person': [], 
-       'company': [] 
-    }
-
-    for row in file_reader:
-        if count == 0:
-            # Вывод строки, содержащей заголовки для столбцов
-            print(f'Файл содержит столбцы: {", ".join(row)}')
-            count += 1
-        else:
-            d = {
-                    'time': row[0],
-                    'person': row[1] + ' ' + row[2], 
-                    'company': row[4] 
-            }
+#Считываем данные из csv файла в структуру dict 
+with open('test.csv', encoding='utf-8') as r_file:
+    reader = csv.reader(r_file, delimiter = ";")
+          
+    for row in reader:
+        if rows_read >= rows_to_skip:            
+           sliced_time_str = time_slicing(row[0]) 
+           
+               
+                   
+           person = row[1] + " " + row[2]
+           address = row[3]
+           company = row[4]
+           buffer_dict = {
+               #'entry_time':entry_time,
+               #'exit_time':exit_time, 
+               'person':person, 
+               'address': address, 
+               'company':company
+            }              
+           time = str_to_datetime(sliced_time_str)
             
-        
-    
-    print(f'Всего в файле {count} строк.')
-    print(d)
-    
+           
+           if person in data:
+               if time < data.get(person).get('buffer_dict').get('entry_time'):
+                    data[person]['buffer_dict']['entry_time'] = time                                  
+               elif time > data[person]['buffer_dict']['exit_time']:
+                    data[person]['buffer_dict']['exit_time'] = time 
+           else:
+                #data[person]['buffer_dict'].append(buffer_dict)
+                data[person] = {'buffer_dict':[buffer_dict]}   
+
+            
+        rows_read += 1
+    # Как настоящий джентльмен, не допускаем утечек
+    r_file.close()
+print(data)
+"""
+for person in data:
+    x = data.get(person)
+    entry_time_min = x["entry_time"]
+    if entry_time < entry_time_min:
+        entry_time_min = entry_time
+"""
